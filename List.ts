435e809c9ad57b126b1a7e9fc58ef0d5e430abad
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-03 00:16:52 +0300

    linked list - dev

--- List.ts
+++ List.ts
@@ -1,37 +1,36 @@
 export class LinkedList<T>{
     private linked_list : { [index:number] : T } = {};
-    public add(arg: T, index: number){
+    public add(arg: T, index: number): void{
         this.linked_list[index] = arg;
     }
-
     public get(index: number){
         return this.linked_list[index];
     }
-
-
     public clear() {
         this.linked_list = {};
     }
-
     public print(){
         for( let key in this.linked_list){
             console.log(key);
         }
     }
-    /*  public remove<T>(index: number): T {
-
-  }
-     public size<T>(): T {
-
+/*
+    public size<T>(){
+        let arg: Array<T> = this.linked_list;
+        console.log(arg.length);  // Array has a .length, so no more error
     }
+    public remove<T>(index: number): T {
+
+     }
+
     public equals<T>(): T {
 
     }
     public hashcode<T>(): T {
 
     }
-    public toString<T>(): T {
-
+    public toString<T>(): string{
+        return this.linked_list.join(" ");
     }
-    */
+*/
 }
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import {LinkedList} from "./List";
 const list = new LinkedList<string>();
 list.add("Mobile",2);
 list.add("products",10);
+list.toString();
 console.log(list.get(2));
 list.print();
 list.add("Polina",2);
