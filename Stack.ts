5e2cf67293a924111786517302ecd322ee4dad69
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-03 02:07:49 +0300

    stack - dev

--- Stack.ts
+++ Stack.ts
@@ -1,23 +1,42 @@
 export class Stack<T>{
-    private stack: T[] = [];
+    private stack : { [index:number] : T } = {};
+    private index:number = 0;
+    //private stack: T[] = [];
 //push, pop, size, equals, hashcode, toString
-
-    public push(): void{
+    public push(arg: T){
+        this.stack[this.index] = arg;
+        this.index++;
+        console.log("Push: " + arg);
+        //Добавление в начало нужно!!!
+    }
+/*
+    public pop(){
 
     }
-    public pop(): T{
-        return this.stack.pop();
-    }
-    public size(){
-    }
-
     public equals<T>(){
 
     }
     public hashcode<T>(){
 
     }
-    public toString<T>(){
+*/
+    public size():number{
+        let arr: Array<T> = [];
+        for (let key in this.stack) {
+            arr.push(this.stack[key]);
+        }
+        console.log("Size: " + arr.length);
+        return arr.length;
 
     }
+    public ToString<T>(): string {
+        let str: string = "";
+        for (let key in this.stack) {
+            str += this.stack[key].toString();
+        }
+        console.log("ToString: " + str);
+        return str;
+    }
+
+
 }
---
+++ index.ts
@@ -0,0 +1,11 @@
+import {Stack} from "./Stack";
+let a: string;
+const stack = new Stack<number>();
+stack.push(9);
+stack.push(4);
+stack.push(1);
+stack.push(8);
+stack.push(9);
+stack.ToString();
+stack.size()
+stack.pop();
