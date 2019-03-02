508a7aeacd1b1a4e21d97423ca88abd76ef790e8
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-03 00:46:19 +0300

    stack - dev

---
+++ Stack.ts
@@ -0,0 +1,23 @@
+export class Stack<T>{
+    private stack: T[] = [];
+//push, pop, size, equals, hashcode, toString
+
+    public push(): void{
+
+    }
+    public pop(): T{
+        return this.stack.pop();
+    }
+    public size(){
+    }
+
+    public equals<T>(){
+
+    }
+    public hashcode<T>(){
+
+    }
+    public toString<T>(){
+
+    }
+}
