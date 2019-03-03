b4f48ab145c8c28ec86a712f59c996391286621e
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-04 01:43:19 +0300

    linked list - dev

--- List.ts
+++ List.ts
@@ -1,22 +1,20 @@
 export class LinkedList<T>{
-   // private linked_list: LinkedList<T>;
-    private linked_list: T[];
-    private key: number;
-    constructor(){
-        this.key = 0;
-        this.linked_list = [];
-    }
+    private linked_list: T[] = [];
+    private key: number = 0;
+
     public add(arg: T): void{
         this.linked_list[this.key] = arg;
         this.key++;
-        console.log("Push " + arg);
     }
+
     public get(index: number): T {
         return this.linked_list[index];
     }
+
     public size<T>():number{
         return this.linked_list.length;
     }
+
     public ToString<T>(): string {
         let str: string = "";
         for (let key in this.linked_list) {
@@ -24,6 +22,7 @@ export class LinkedList<T>{
         }
         return str;
     }
+
     public equals<T>(arg: LinkedList<T>): boolean {
         if(this.size() == arg.size()) {
                 if(this.ToString() !== arg.ToString() ) {
@@ -31,7 +30,9 @@ export class LinkedList<T>{
                 }
             return true;
         }
+        return false;
     }
+
     public hashcode<T>(): number {
         let hash = 0;
         for(let i=0;i<this.size();i++){
@@ -40,25 +41,23 @@ export class LinkedList<T>{
         }
         return hash;
     }
+
+
+
+
     public remove(index: number){
+        let listick: LinkedList<T>;
         if(this.size() == 0 || index-1 > this.size()) {
             console.log("Error");
         }
-        let listik: LinkedList<T>;
-        console.log("LISTICK " + listik);
         let check: number = 0;
-        for( let i in this.linked_list){
+        for (let i in this.linked_list) {
             console.log(i);
-            if (check == index-1){
-                continue;
+            if (check == index - 1) {
+                 continue;
             }
-            console.log("get(check): " + this.get(check));
-
-            listik.add(this.get(check));
+            listick.add(this.get(check));
             check++;
-
         }
-
-
     }
 }
