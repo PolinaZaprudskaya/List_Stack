d14b2169ea0bc605d8dabdf33ae68471d1fe9daf
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-03 15:48:15 +0300

    stack - dev

--- Stack.ts
+++ Stack.ts
@@ -4,21 +4,29 @@ export class Stack<T>{
     private index:number = 0;
     //push, pop, size, equals, hashcode, toString
     public push(arg: T){
-        this.stack[this.index] = arg;
+      /*  this.stack[this.index] = arg;
         this.index++;
         console.log("Push: " + arg);
+        */
+      console.log("Push: " + arg);
+      this.stack.push(arg);
     }
     public size<T>():number{
-        console.log("Size: " + Object.keys(this.stack).length);
-        return Object.keys(this.stack).length;
+       /* console.log("Size: " + Object.keys(this.stack).length);
+        return Object.keys(this.stack).length;*/
+        console.log("Size: " + this.stack.length);
+        return this.stack.length;
     }
     public ToString<T>(): string {
-        let str: string = "";
+      /*  let str: string = "";
         for (let key in this.stack) {
             str += this.stack[key].toString();
         }
         console.log("ToString: " + str);
         return str;
+        */
+        console.log("ToString: " + this.stack.join(""));
+        return this.stack.join("");
     }
     public pop(){
         console.log("---pop---");
