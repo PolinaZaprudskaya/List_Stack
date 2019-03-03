754d73efe14310a425f937d5f67ded8c3523856b
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-04 00:17:53 +0300

    linked list - dev

--- List.ts
+++ List.ts
@@ -1,24 +1,29 @@
 export class LinkedList<T>{
-    private linked_list: T[] = [];
-    public add(arg: T, index: number): void{
-        this.linked_list[index] = arg;
+    private linked_list: LinkedList<T>;
+    private key: number = 0;
+    public add(arg: T): void{
+        this.linked_list[this.key] = arg;
+        this.key++;
     }
     public get<T>(index: number){
         console.log("Get: " + this.linked_list[index]);
         return this.linked_list[index];
     }
     public size<T>():number{
-        console.log("Size: " + this.linked_list.length);
-        return this.linked_list.length;
+        return Object.keys(this.linked_list).length;
+
     }
     public ToString<T>(): string {
-        console.log("ToString: " + this.linked_list.join(""));
-        return this.linked_list.join("");
+        let str: string = "";
+        for (let key in this.linked_list) {
+            str += this.linked_list[key].toString();
+        }
+        return str;
     }
     public equals<T>(arg: LinkedList<T>): boolean {
-        if(this.linked_list.length == arg.linked_list.length) {
+        if(this.linked_list.size() == arg.linked_list.size()) {
             for (let key in this.linked_list) {
-                if((this.linked_list[key]).toString() !== (arg.linked_list[key]).toString() ) {
+                if((this.linked_list[key]).ToString() !== (arg.linked_list[key]).ToString() ) {
                     return false;
                 }
             }
@@ -27,13 +32,28 @@ export class LinkedList<T>{
     }
     public hashcode<T>(): number {
         let hash = 0;
-        for(let i=0;i<this.linked_list.length;i++){
-            hash += Math.pow((this.linked_list.join("")).charCodeAt(i)*31, this.linked_list.length - i);
+        for(let i=0;i<this.linked_list.size();i++){
+            hash += Math.pow((this.linked_list.ToString()).charCodeAt(i)*31, this.linked_list.size() - i);
             hash = hash&hash;
         }
         return hash;
     }
     public remove<T>(index: number){
+        if(this.linked_list.size() == 0 || index-1 > this.linked_list.size()) {
+            console.log("Error");
+        }
+        let listik: LinkedList<T>;
+        let check: number = 0;
+        for( let key in this.linked_list){
+            console.log(key);
+            if (check == index-1){
+                continue;
+            }
+            listik.linked_list.add(this.linked_list.get(check));
+            check++;
+
+        }
+
 
     }
 }
--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import {LinkedList} from "./List";
 
 const list = new LinkedList<string>();
-list.add("Mobile", 2);
-list.add("TV", 10);
+list.add("Mobile");
+list.add("TV");
+list.add("P");
+list.add("D");
 list.ToString();
 list.get(2);
 list.size();
 
 const list_2 = new LinkedList<string>();
-list_2.add("Mobile", 2);
-list_2.add("TV", 10);
+list_2.add("Mobile");
+list_2.add("TV");
 
 
 
 console.log("Equals: " + list.equals(list_2));
 console.log("Hashcode: " + list.hashcode());
+list.remove(2);
+list.ToString();
