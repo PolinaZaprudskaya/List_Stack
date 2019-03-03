5b4b76eb4f535131d2cd771dd198559a9ee05c67
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-03 22:22:57 +0300

    linked list - dev

--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@ import {LinkedList} from "./List";
 
 const list = new LinkedList<string>();
 list.add("Mobile", 2);
-list.add("products", 10);
+list.add("TV", 10);
 list.ToString();
 list.get(2);
 list.size();
 
 const list_2 = new LinkedList<string>();
 list_2.add("Mobile", 2);
-list_2.add("Pol", 10);
+list_2.add("TV", 10);
 
 
 
