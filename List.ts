573df7a4f1b01f90e3fdf2357c8e01e7e53a7dce
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-03 14:40:10 +0300

    linked list - dev

--- List.ts
+++ List.ts
@@ -1,36 +1,36 @@
 export class LinkedList<T>{
-    private linked_list : { [index:number] : T } = {};
+    private linked_list : { [index:number] : T } = {}
+
     public add(arg: T, index: number): void{
         this.linked_list[index] = arg;
+        console.log("Add: : " + arg);
     }
     public get(index: number){
+        console.log("Get: " + this.linked_list[index]);
         return this.linked_list[index];
     }
-    public clear() {
-        this.linked_list = {};
+    public size<T>():number{
+        console.log("Size: " + Object.keys(this.linked_list).length);
+        return Object.keys(this.linked_list).length;
     }
-    public print(){
-        for( let key in this.linked_list){
-            console.log(key);
+    public ToString<T>(): string {
+        let str: string = "";
+        for (let key in this.linked_list) {
+            str += this.linked_list[key].toString();
         }
+        console.log("ToString: " + str);
+        return str;
     }
+
 /*
-    public size<T>(){
-        let arg: Array<T> = this.linked_list;
-        console.log(arg.length);  // Array has a .length, so no more error
-    }
     public remove<T>(index: number): T {
 
      }
-
     public equals<T>(): T {
 
     }
     public hashcode<T>(): T {
 
-    }
-    public toString<T>(): string{
-        return this.linked_list.join(" ");
     }
 */
 }
--- index.ts
+++ index.ts
@@ -3,13 +3,7 @@ import {LinkedList} from "./List";
 const list = new LinkedList<string>();
 list.add("Mobile",2);
 list.add("products",10);
-list.toString();
-console.log(list.get(2));
-list.print();
-list.add("Polina",2);
-console.log(list.get(2));
-list.clear();
+list.ToString();
+list.get(2);
+list.size();
 
-const list_string = new LinkedList<string>();
-list_string.add("Node Js ",4);
-list_string.print();
