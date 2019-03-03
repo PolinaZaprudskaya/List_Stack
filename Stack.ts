1f2fabae22c99dc2be058822ad8dfd2fd31c68f5
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-03-03 22:31:03 +0300

    Stack - dev

--- Stack.ts
+++ Stack.ts
@@ -1,43 +1,34 @@
 export class Stack<T>{
-   // private stack : { [index:number] : T } = {};
     private stack: T[] = [];
-    private index:number = 0;
-    //push, pop, size, equals, hashcode, toString
     public push(arg: T){
-      /*  this.stack[this.index] = arg;
-        this.index++;
-        console.log("Push: " + arg);
-        */
-      console.log("Push: " + arg);
       this.stack.push(arg);
     }
     public size<T>():number{
-       /* console.log("Size: " + Object.keys(this.stack).length);
-        return Object.keys(this.stack).length;*/
-        console.log("Size: " + this.stack.length);
         return this.stack.length;
     }
     public ToString<T>(): string {
-      /*  let str: string = "";
-        for (let key in this.stack) {
-            str += this.stack[key].toString();
-        }
-        console.log("ToString: " + str);
-        return str;
-        */
-        console.log("ToString: " + this.stack.join(""));
         return this.stack.join("");
     }
     public pop(){
-        console.log("---pop---");
-        return this.stack.pop();
+       return this.stack.pop();
     }
-/*      public equals<T>(){
-
+    public equals<T>(arg: Stack<T>): boolean {
+        if(this.stack.length == arg.stack.length) {
+            for (let key in this.stack) {
+                if((this.stack[key]).toString() !== (arg.stack[key]).toString() ) {
+                    return false;
+                }
+            }
+            return true;
         }
-        public hashcode<T>(){
-
+    }
+    public hashcode<T>(): number {
+        let hash = 0;
+        for(let i=0;i<this.stack.length;i++){
+            hash += Math.pow((this.stack.join("")).charCodeAt(i)*31, this.stack.length - i);
+            hash = hash&hash;
         }
-    */
+        return hash;
+    }
 
 }
--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 import {Stack} from "./Stack";
-let a: number;
+
 const stack = new Stack<number>();
 stack.push(9);
 stack.push(4);
 stack.push(1);
 stack.push(8);
 stack.push(9);
-stack.ToString();
-stack.size();
-
+console.log("ToString: " + stack.ToString());
+console.log("Hashcode: " + stack.hashcode());
+console.log("Size: " + stack.size());
 stack.pop();
-stack.ToString()
-stack.size();
+console.log("-----------");
+console.log("ToString: " + stack.ToString());
+console.log("Size: " + stack.size());
+console.log("Hashcode: " + stack.hashcode());
+
+const stack_1 = new Stack<number>();
+stack_1.push(9);
+stack_1.push(4);
+stack_1.push(1);
+stack_1.push(8);
+
+console.log("Equals: " + stack.equals(stack_1))
